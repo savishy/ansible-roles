0ab89f6bf4fd1c26d320f0493f6819cb72867cf4
Vish <savishy@users.noreply.github.com>
2018-09-24 10:04:15 +0530

    Update run.sh

--- controller/run.sh
+++ controller/run.sh
@@ -1,2 +1,2 @@
-ansible-galaxy install http://github.com/savishy/ansible-roles.git --force
+ansible-galaxy install http://github.com/savishy/ansible-roles --force
 ansible-playbook --ask-vault-pass -vv playbook.yml
