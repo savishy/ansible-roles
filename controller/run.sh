94e1f9cf77c21749a75640fb13336c3df35c4cac
Vish <savishy@users.noreply.github.com>
2018-09-24 10:05:45 +0530

    Update run.sh
    script to run playbook that creates an ansible controller in azure. is meant to be run from azure cloud shell.

--- controller/run.sh
+++ controller/run.sh
@@ -1,2 +1,2 @@
-ansible-galaxy install http://github.com/savishy/ansible-roles --force
+ansible-galaxy install git+http://github.com/savishy/ansible-roles --force
 ansible-playbook --ask-vault-pass -vv playbook.yml
